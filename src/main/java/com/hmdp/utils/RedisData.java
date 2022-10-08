d59bef7c5b6959ddb1e5f41d866f7cc27e6105ab
cs <1006596474@qq.com>
2022-10-08 19:26:36 +0800

    2022-10-08 commit1 完成商户查询缓存

--- src/main/java/com/hmdp/utils/RedisData.java
+++ src/main/java/com/hmdp/utils/RedisData.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * Redis数据
+ *
+ * @author CHEN
+ * @date 2022/10/08
+ */
 @Data
 public class RedisData {
     private LocalDateTime expireTime;
