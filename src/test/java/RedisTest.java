b9026b6da2274f4fc7f419aceb6d84c8e24222b7
cs <1006596474@qq.com>
2022-10-20 00:57:12 +0800

    2022-10-20 commit2 UV统计测试（完结）

--- src/test/java/RedisTest.java
+++ src/test/java/RedisTest.java
@@ -103,4 +103,19 @@ public class RedisTest {
             stringRedisTemplate.opsForGeo().add(key,locations);
         }
     }
+
+    @Test
+    public void testHyperLogLog(){
+        String[] values=new String[1000];
+        int j=0;
+        for (int i = 0; i < 1000000; i++) {
+            j=i%1000;
+            values[j]="user_"+i;
+            if (j==999){
+                stringRedisTemplate.opsForHyperLogLog().add("hl1",values);
+            }
+        }
+        Long hl1 = stringRedisTemplate.opsForHyperLogLog().size("hl1");
+        System.out.println(hl1);
+    }
 }
