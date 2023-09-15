304abf57e644f56b19549cfa7f8da930f1328870
Blacktuna1 <93248801+Blacktuna1@users.noreply.github.com>
2023-09-15 14:41:54 +0800

    UserHolder的userDto改回去

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +19,7 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
         // 保存用户信息到ThreadLocal
-        UserHolder.saveUser((User)user);
+        UserHolder.saveUser((UserDTO)user);
 
         return true;
 
