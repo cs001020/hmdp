5db5589c01ac1b561fdd09a8b1d57fb68ffa0c78
Blacktuna1 <93248801+Blacktuna1@users.noreply.github.com>
2023-09-15 14:24:30 +0800

    loginInterceptor

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,19 @@
+package com.hmdp.utils;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
